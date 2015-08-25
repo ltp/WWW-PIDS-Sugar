f1fb8ca3910c39cdf99ba5b1bc42511c6ee1d898
Luke Poskitt <luke.poskitt@gmail.com>
2015-08-25 16:21:59 +1000

    Added some base methods.

--- lib/WWW/PIDS/Sugar/Route.pm
+++ lib/WWW/PIDS/Sugar/Route.pm
@@ -3,11 +3,23 @@ package WWW::PIDS::Sugar::Route;
 use strict;
 use warnings;
 
+use overload ( '""' => \&__to_string );
+
 sub new {
 	my ( $class, %args ) = @_;
 	
 	my $self = bless {}, $class;
 	return $self
 }
+
+sub route_no	{ return $_[0]->{RouteNo}		}
+
+sub description	{ return $_[0]->{Description}		}
+
+sub up		{ return $_[0]->{UpDestination}		}
+
+sub down	{ return $_[0]->{DownDestination}	}
+
+sub __to_string	{ return $_[0]->{RouteNo}		}
 
 1;
